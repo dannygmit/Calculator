a58dd0b1e387c18285b2b73bb382afabce19aa7a
dannygmit <g00350847@gmit.ie>
2020-01-30 11:36:39 +0000

    This is my commit

---
+++ src/main/java/gmit/Calculator.java
@@ -0,0 +1,26 @@
+package gmit;
+
+public class Calculator {
+
+    private int firstNum;
+    private int secondNum;
+
+    public int getFirstNum() {
+        return firstNum;
+    }
+
+    public void setFirstNum(int firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public int getSecondNum() {
+        return secondNum;
+    }
+
+    public void setSecondNum(int secondNum) {
+        this.secondNum = secondNum;
+    }
+
+}
+
+
